1b02e235b2e5971d77274b4162d6d58650b038c9
johneliades <johneliades@hotmail.com>
2020-12-06 18:21:35 +0200

    code refactoring

--- code_crawler.py
+++ code_crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from googlesearch import search 
 import urllib3
 import sys
+import os
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,13 +16,15 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-available_sites = ["w3schools", "stackoverflow"]
+available_sites = ["w3schools", "stackoverflow", "tutorialspoint", "geeksforgeeks"]
 
 try:
 	query = sys.argv[1]
 except:
 	print("Give search query as argument")
 	sys.exit()
+
+rows, columns = os.popen('stty size', 'r').read().split()
 
 http = urllib3.PoolManager()
 print()
@@ -31,26 +34,36 @@ for url in search(query, tld="co.in", num=10, stop=10, pause=2):
 
 		site = [x for x in available_sites if url.find(x)!=-1][0]
 
-		if(site == "w3schools"):
+		if(site in available_sites):
 			response = http.request('GET', url)
 			soup = BeautifulSoup(response.data, features="lxml")
-			result = soup.find("div", {"class": "w3-code"})
-			result = result.get_text(separator="\n").strip()
-			print(bcolors.OKCYAN + "w3schools: " + bcolors.FAIL + url + bcolors.ENDC) 
-			print("-------------------------------------------")
-			print(result)
-			if(not result.endswith("\n")):
-				print()
-		elif(site == "stackoverflow"):
-			response = http.request('GET', url)
-			soup = BeautifulSoup(response.data, features="lxml")
-			result = soup.find("div", {"class": "accepted-answer"})
-			result = result.find("div", {"class": "s-prose"})
-			result = result.find("pre").find(text=True)
-			print(bcolors.OKCYAN + "stackoverflow: " + bcolors.FAIL + url + bcolors.ENDC) 
-			print("-------------------------------------------")
-			print(result)
-			if(not result.endswith("\n")):
+
+			if(site == "w3schools"):
+				result = soup.find("div", {"class": "w3-code"})
+				result = result.get_text(separator="\n").strip()
+			elif(site == "stackoverflow"):
+				result = soup.find("div", {"class": "accepted-answer"})
+				result = result.find("div", {"class": "s-prose"})
+				result = result.find("pre").find(text=True)
+			elif(site == "tutorialspoint"):
+				result = soup.find("div", {"class": "tutorial-content"})
+				result = result.find("pre").find(text=True)	
+			elif(site == "geeksforgeeks"):
+				result = soup.find("td", {"class": "code"})
+				result = result.get_text().strip()
+
+			print(bcolors.OKCYAN + site + ": " + bcolors.FAIL + url + bcolors.ENDC) 
+			
+			for i in range(int(columns)):
+				print(u'\u2500', end="")
+
+			print()
+
+			lines = result.splitlines()
+			for line in lines:
+				print("   " + line)
+
+			if(not line.endswith("\n")):
 				print()
 
 	except:
