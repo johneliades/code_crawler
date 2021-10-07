5ce78fa09a38f727708af350743b95ab2b6d4b3e
johneliades <johneliades@hotmail.com>
2021-10-07 22:55:32 +0300

    used guesslang

--- code_crawler.py
+++ code_crawler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import threading
 from bs4 import BeautifulSoup
 from googlesearch import search 
 from pygments import lexers, highlight
@@ -8,7 +7,7 @@ from pygments.formatters import TerminalFormatter
 import urllib3
 import sys
 import random
-import Algorithmia
+from guesslang import Guess
 import os
 
 class bcolors:
@@ -25,39 +24,25 @@ try:
 except:
 	query = input("Give search query: ")
 
-try:
-	keyFile = open(os.path.dirname(os.path.realpath(__file__)) + '/api_key', 'r')
-	api_key = keyFile.readline().strip()
-except:
-	print(bcolors.RED)
-	print("Please get a free API key by signing up and write it in a ", end="")
-	print("file called \"api_key\" in the same folder.")
-	print(bcolors.CYAN)
-	print("https://algorithmia.com/algorithms/PetiteProgrammer/ProgrammingLanguageIdentification")
-	print(bcolors.ENDC)
-	exit()
-
-client = Algorithmia.client(api_key)
-algo = client.algo('PetiteProgrammer/ProgrammingLanguageIdentification/0.1.3')
-
 print()
 
 num_results = 7
 http = urllib3.PoolManager()
-mutex = threading.Lock()
 total_results = []
-threads = []
+guess = Guess()
 
-def find_answer(url):
+for url in search(query, tld="com", lang='en', num=num_results, stop=num_results, pause=random.uniform(0, 1)): 
 	site = [x for x in available_sites if url.find(x)!=-1]
 	if(len(site)!=0):
 		site = site[0]
 	else:
-		return
+		continue
 
 	if(site in available_sites):
 		response = http.request('GET', url)
 		soup = BeautifulSoup(response.data, features="html.parser")
+
+		result = ""
 
 		try:
 			if(site == "w3schools"):
@@ -88,49 +73,30 @@ def find_answer(url):
 			if result not in total_results:
 				total_results.append(result)
 			else:
-				return
-
+				continue
 		except:
-			return
-    
-		try:
-			code_lang = algo.pipe(result).result[0]
-		except(Algorithmia.errors.AlgorithmException):
-			print(bcolors.RED)
-			print("API key in file \"api_key\" is wrong, check again")
-			print(bcolors.ENDC)
-			exit()
+			continue
 
-		language = code_lang[0]
-		probability = code_lang[1]
+		language = guess.language_name(result)
+		probability = guess.probabilities(result)[0][1]
 
-		with mutex:
-			print(bcolors.CYAN + bcolors.BOLD + site + ": " + bcolors.RED + url + bcolors.ENDC) 
-			for i in range(min(80, len(site + ": " + url))):
-				print(u'\u2501', end="")
-			print()
+		print(bcolors.CYAN + bcolors.BOLD + site + ": " + bcolors.RED + url + bcolors.ENDC) 
+		for i in range(min(80, len(site + ": " + url))):
+			print(u'\u2501', end="")
+		print()
 
-		#	print(bcolors.CYAN + language + " " + 
-		#		str(round(probability, 3) * 100) + "% certainty" + bcolors.ENDC)
-		#	for i in range(len(language + str(round(probability, 3) * 100)) + 12):
-		#		print(u'\u2500', end="")
-		#	print()
+		# print(bcolors.CYAN + language + " " + 
+		# 	str(round(probability, 3) * 100) + "% certainty" + bcolors.ENDC)
+		# for i in range(len(language + str(round(probability, 3) * 100)) + 12):
+		# 	print(u'\u2500', end="")
+		# print()
 
-			if(language == "markdown"):
-				language = "md"
-			elif(language == "vb"):
-				language = "basic"
+		if(language == "markdown"):
+			language = "md"
+		elif(language == "vb"):
+			language = "basic"		
+		elif(language == "Batchfile"):
+			language = "batch"
 
-			lexer = lexers.get_lexer_by_name(language)
-			print(highlight(result, lexer, TerminalFormatter()))
-
-once = True
-
-for url in search(query, tld="com", lang='en', num=num_results, stop=num_results, pause=random.uniform(0, 1)): 
-	if(once):
-		find_answer(url)
-		once = False
-		continue
-	t = threading.Thread(target=find_answer, args=(url,))
-	threads.append(t)
-	t.start()
+		lexer = lexers.get_lexer_by_name(language)
+		print(highlight(result, lexer, TerminalFormatter()))
